9d32877f20c7729e7ec0f1fbb1e717cfa16569a8
PuchkovaDasha <43794970+PuchkovaDasha@users.noreply.github.com>
2018-10-13 15:57:37 +0300

    Update test4.py

--- test4.py
+++ test4.py
@@ -3,6 +3,6 @@ n = str(input())
 k1 = k.split(" ")
 n1 = n.split(" ")
 def skpr(k, n):
-      return sum([k1[i] * k2[i] for i in range(len(k1))])
+      return sum([k1[i] * n1[i] for i in range(len(k1))])
 
 
