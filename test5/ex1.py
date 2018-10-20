77917d2d12b66c11f81327d6a4152753bedc9dfa
PuchkovaDasha <darya.puchkova@phystech.edu>
2018-10-20 16:05:00 +0300

    initial commit

--- test5/ex1.py
+++ test5/ex1.py
@@ -1,5 +1,8 @@
 import math
-list_ = [math.factorial(i) for i in range (1,101) if 2 in ]
+list_ = [math.factorial(i) for i in range (1,101)]
+print(list_)
+
 
 import math
 l2 = [math.factorial(i) for i in range(1, 101) if str(math.factorial(i))[0] == 2]
+print(l2)
